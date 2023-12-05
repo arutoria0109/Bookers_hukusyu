32e5ac427841fee3865f8cde9a4d4caee05167d2
EC2 Default User <samanimaya017@gmail.com>
2023-12-05 10:56:25 +0000

    [add]controller追記

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,34 @@
 class BooksController < ApplicationController
-  
+
   def index
     @book = Book.new
     @books = Book.all
   end
+
+  def create
+    book = Book.new(book_paramus)
+    book.save
+    redirect_to books_path
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+  
+  private
+
+  def book_paramus
+    params.require(:book).permit(:title, :body)
+  end
 end
+
+
