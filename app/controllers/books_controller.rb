f90c2c508e115f5e280e8044e02f5df04ca78867
EC2 Default User <samanimaya017@gmail.com>
2023-12-06 18:39:40 +0000

    edit,show.destroy controller追記

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,27 +6,38 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_paramus)
-    book.save
+    @book = Book.new(book_params)
+    @book.save
+    flash[:notice] = "Book was successfully created."
+    redirect_to book_path(@book.id)
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
+    redirect_to book_path(@book.id)
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to books_path
   end
-  
-  def show
-  end
-  
-  def edit
-  end
-  
-  def update
-  end
-  
-  def destroy
-  end
-  
-  
+
+
   private
 
-  def book_paramus
+  def book_params
     params.require(:book).permit(:title, :body)
   end
 end
